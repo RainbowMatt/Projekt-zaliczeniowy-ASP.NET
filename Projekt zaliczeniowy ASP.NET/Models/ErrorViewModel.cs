de35bcce57d7464dada9a60e7bfb6bc4669ee087
Mateusz Witulski <witulskimateusz@outlook.com>
2023-01-23 12:05:51 +0100

    Dodaj pliki projektów.

---
+++ Projekt zaliczeniowy ASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projekt_zaliczeniowy_ASP.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
